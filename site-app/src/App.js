d02ecef643f473a668809d2557e859aeba68b10c
Jeremy Cummins <jjcummins@outlook.com>
2018-10-20 23:39:11 -0700

    Add Color Picker

--- site-app/src/App.js
+++ site-app/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SliderPicker } from 'react-color'; 
 
 class App extends Component {
+  state = {
+    background: "blue",
+  };
+
+  handleChangeComplete = (color) => {
+    this.setState({ background: color.hex });
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App" style={{background: this.state.background}}>
+        <header className="App-header" >
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Welcome to William's Costume Site!
           </p>
+          <SliderPicker color={ this.state.background } onChangeComplete={ this.handleChangeComplete }/>
         </header>
       </div>
-    );
-  }
+      );
+    }
 }
 
 export default App;
